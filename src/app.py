fd0bb4c59961b5082ae36023eff16a6ccd75833e
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-11-06 19:02:23 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -30,13 +30,47 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def post_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'})
+    if 'first_name' not in body:
+        return jsonify({'msg': 'El campo first_name es obligatorio'})
+    if 'age' not in body:
+        return jsonify({'msg': 'El campo age es obligatorio'})
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg': 'El campo lucky_numbers es obligatorio'})
+    #para crear un nuevo miembro:
+    new_member = {
+        'id': jackson_family._generateId(),
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+
+    members = jackson_family.add_member(new_member)
+    return jsonify({'msg': 'OK', 'members': members})
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_a_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_a_member(id): 
+    member = jackson_family.get_member(id)
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({'msg': 'Se eliminó el miembro con el id proporcionado'}), 200
+    else:
+        return jsonify({'msg': 'No existe miembro con el id proporcionado'}), 400
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
